94e104170505d9bf8e968082ca18a0a58d4ddb3e
afifrohul <afifmemyself22@gmail.com>
2025-10-21 09:52:07 +0700

    setup database

---
+++ app/Models/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'product_category_id',
+        'name',
+        'slug',
+        'description',
+        'base_price',
+        'status',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(ProductCategory::class, 'product_category_id');
+    }
+
+    public function variants()
+    {
+        return $this->hasMany(ProductVariant::class);
+    }
+
+    public function galleries()
+    {
+        return $this->hasMany(ProductGallery::class);
+    }
+}
