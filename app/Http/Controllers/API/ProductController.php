0c546a811c850e6f5a6a767b0f07e54ed8296570
afifrohul <afifmemyself22@gmail.com>
2025-11-06 22:17:52 +0700

    update products API mapping to get price from variants

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
     {
         $search = $request->get('search');
 
-        $products = Product::with(['category', 'variants', 'galleries'])->where('status', 'released');
+        $products = Product::with(['category', 'variants'])->where('status', 'released');
 
         if ($search) {
             $products->where('name', 'like', '%' . $search . '%');
@@ -27,7 +27,23 @@ class ProductController extends Controller
             return $this->errorResponse('No products found.', null, 404);
         }
 
-        return $this->successResponse($products, 'Products fetched successfully.');
+        $products = $products->map(function ($product) {
+        return [
+            'id' => $product->id,
+            'product_category_id' => $product->product_category_id,
+            'image' => $product->image,
+            'name' => $product->name,
+            'slug' => $product->slug,
+            'description' => $product->description,
+            'status' => $product->status,
+            'created_at' => $product->created_at,
+            'updated_at' => $product->updated_at,
+            'category' => $product->category,
+            'price' => optional($product->variants->first())->price,
+        ];
+    });
+
+    return $this->successResponse($products, 'Products fetched successfully.');
     }
 
     public function show($id)
