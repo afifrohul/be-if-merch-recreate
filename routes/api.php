73ded97bcadf9b7aab8276949fabe92cf88becc7
afifrohul <afifmemyself22@gmail.com>
2025-11-13 10:57:26 +0700

    update API to V1

--- routes/api.php
+++ routes/api.php
@@ -2,41 +2,39 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\FAQController;
-use App\Http\Controllers\API\ProductCategoryController;
-use App\Http\Controllers\API\ProductController;
-use App\Http\Controllers\API\CartController;
-use App\Http\Controllers\API\TransactionController;
-use App\Http\Controllers\API\ProfileController;
+use App\Http\Controllers\API\V1\AuthController;
+use App\Http\Controllers\API\V1\FAQController;
+use App\Http\Controllers\API\V1\ProductCategoryController;
+use App\Http\Controllers\API\V1\ProductController;
+use App\Http\Controllers\API\V1\CartController;
+use App\Http\Controllers\API\V1\TransactionController;
+use App\Http\Controllers\API\V1\ProfileController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'register']);
-Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
-Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout'])->middleware('auth:sanctum');
-
-Route::get('/faqs', [FAQController::class, 'index']);
-
-Route::get('/product-categories', [ProductCategoryController::class, 'index']);
-
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/product/{slug}', [ProductController::class, 'show']);
-
-Route::middleware(['auth:sanctum'])->group(function () {
-
-    Route::get('/user', [ProfileController::class, 'index']);
-    Route::post('/profile', [ProfileController::class, 'store']);
+Route::prefix('V1')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
     
-    Route::get('/cart', [CartController::class, 'index']);
-    Route::post('/cart', [CartController::class, 'store']);
-    Route::delete('/cart/{id}', [CartController::class, 'destroy']);
-
-    Route::get('/transactions', [TransactionController::class, 'index']);
-    Route::get('/transaction/{id}', [TransactionController::class, 'show']);
-    Route::post('/transactions', [TransactionController::class, 'store']);
+    Route::get('/faqs', [FAQController::class, 'index']);
+    
+    Route::get('/product-categories', [ProductCategoryController::class, 'index']);
+    
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/product/{slug}', [ProductController::class, 'show']);
+    
+    Route::middleware(['auth:sanctum'])->group(function () {
+    
+        Route::get('/user', [ProfileController::class, 'index']);
+        Route::post('/profile', [ProfileController::class, 'store']);
+        
+        Route::get('/cart', [CartController::class, 'index']);
+        Route::post('/cart', [CartController::class, 'store']);
+        Route::delete('/cart/{id}', [CartController::class, 'destroy']);
+    
+        Route::get('/transactions', [TransactionController::class, 'index']);
+        Route::get('/transaction/{id}', [TransactionController::class, 'show']);
+        Route::post('/transactions', [TransactionController::class, 'store']);
+    });
+    
+    Route::post('/midtrans/callback', [TransactionController::class, 'callback']);
 });
-
-Route::post('/midtrans/callback', [TransactionController::class, 'callback']);
